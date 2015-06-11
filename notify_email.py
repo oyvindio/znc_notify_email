a2f18e8813ef2e07ee57e92fe4933f740cf5b6d8
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2015-06-11 21:02:03 +0200

    Initial
    finally got around to parameterizing api key etc.

---
+++ notify_email.py
@@ -0,0 +1,129 @@
+import datetime
+import json
+import re
+import traceback
+
+import znc
+import requests
+
+def should_notify(notify_on, msg):
+    return any((thing in msg for thing in notify_on))
+
+def now_timestamp():
+    return datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+def create_PrivMsg_email_subject(network, nick):
+    return 'ZNC notify_email: {nick} is trying to reach you on {network}'.format(network=network, nick=nick)
+
+def create_PrivMsg_email_body(network, nick, msg, timestamp):
+    return """
+    Hello,
+
+    {nick} is trying to reach you on {network}, but you are currently AWAY.
+    Here is the message:
+
+    {time} <{nick}>: {msg}
+
+    --
+    znc notify_email.py module
+    """.format(network=network, nick=nick, msg=msg, time=timestamp)
+
+
+def create_ChanMsg_email_subject(network, nick, channel):
+    return 'ZNC notify_email: {nick} is trying to reach you in {channel} on {network}'.format(
+        network=network, nick=nick, channel=channel)
+
+def create_ChanMsg_email_body(network, nick, channel, msg, timestamp):
+    return """
+    Hello,
+
+    {nick} is trying to reach you in {channel} on {network}, but you are currently AWAY.
+    Here is the message:
+
+    {time} <{nick}>: {msg}
+
+    --
+    znc notify_email.py module
+    """.format(network=network, nick=nick, channel=channel, msg=msg, time=timestamp)
+
+class notify_email(znc.Module):
+    description = 'Notify highlights and private messages by email'
+
+    def __print_stacktrace_on_error(function):
+        def function_wrapper(self, *args, **kwargs):
+            try:
+                return function(self, *args, **kwargs)
+            except:
+                self.PutModule('Caught exception:')
+                self.PutModule(traceback.format_exc())
+        return function_wrapper
+
+    @__print_stacktrace_on_error
+    def OnLoad(self, args_string, message):
+        args = re.split('\s+', args_string.strip())
+        if len(args) >= 5:
+            api_url, api_key, sender, recipient, *notify_on = args
+            self.mailgun_api_url = api_url
+            self.mailgun_api_key = api_key
+            self.mailgun_sender = sender
+            self.mailgun_recipient = recipient
+            self.notify_on = notify_on
+
+            self.PutModule('Set mailgun_api_url: {}'.format(self.mailgun_api_url))
+            self.PutModule('Set mailgun_api_key: {}'.format(self.mailgun_api_key))
+            self.PutModule('Set mailgun_sender: {}'.format(self.mailgun_sender))
+            self.PutModule('Set mailgun_recipient: {}'.format(self.mailgun_recipient))
+            self.PutModule('Set notify_on: {}'.format(self.notify_on))
+            return True
+        else:
+            self.PutModule('Requires at least 5 arguments: mailgun_api_url mailgun_api_key mailgun_sender mailgun_recipient notify_on [notify_on ...]')
+            return False
+
+
+    @__print_stacktrace_on_error
+    def OnChanMsg(self, nick, channel, msg):
+        if self.isAway() and should_notify(self.notify_on, msg.s):
+            self.PutModule('{} said {} in {}, notifying {}'.format(nick.GetNick(), msg.s, channel.GetName(),
+                                                                   mailgun_recipient))
+            email_subject = create_ChanMsg_email_subject(self.currentNetworkName(), nick.GetNick(), channel.GetName())
+            email_body = create_PrivMsg_email_body(self.currentNetworkName(), nick.GetNick(), channel.GetName(),
+                                                   msg.s, now_timestamp())
+            self.send_mailgun_email(self.mailgun_api_url, self.mailgun_api_key, self.mailgun_sender,
+                                    self.mailgun_recipient, email_subject, email_body)
+
+        return znc.CONTINUE
+
+    @__print_stacktrace_on_error
+    def OnPrivMsg(self, nick, msg):
+        if self.isAway():
+            self.PutModule('{} said {} in query, notifying {}'.format(nick.GetNick(), msg.s, mailgun_recipient))
+            email_subject = create_PrivMsg_email_subject(self.currentNetworkName(), nick.GetNick())
+            email_body = create_PrivMsg_email_body(self.currentNetworkName(), nick.GetNick(), msg.s, now_timestamp())
+            self.send_mailgun_email(self.mailgun_api_url, self.mailgun_api_key, self.mailgun_sender,
+                                    self.mailgun_recipient, email_subject, email_body)
+        return znc.CONTINUE
+
+    def send_mailgun_email(self, base_url, api_key, sender, recipient, subject, body):
+        url = base_url + 'messages' if base_url.endswith('/') else base_url + '/messages'
+        payload = {
+            'from': sender,
+            'to': recipient,
+            'subject': subject,
+            'text': body
+        }
+
+        res = requests.post(url, auth=('api', api_key), data=payload)
+        if res.ok:
+            res_body = json.loads(res.text)
+            self.PutModule('Successfully notified {}, mailgun id={}'.format(recipient, res_body['id']))
+            return True
+        else:
+            error = '{status} {reason}\n{text}'.format(status=res.status, reason=res.reason, text=res.text)
+            self.PutModule('Error while notifying {}, response from mailgun: {}'.format(recipient, error))
+            return False
+
+    def isAway(self):
+        return self.GetNetwork().IsIRCAway()
+
+    def currentNetworkName(self):
+        return self.GetNetwork().GetName()
---
+++ test/test_email_notify.py
@@ -0,0 +1,125 @@
+# dirty hack to make the parts of the znc module we're using importable
+import imp
+import sys
+znc = imp.new_module('znc')
+znc_code = """
+CONTINUE = 'CONTINUE'
+class Module(object):
+    def GetNetwork(self):
+        pass
+    def PutModule(self, msg):
+        pass
+"""
+exec(znc_code, znc.__dict__)
+sys.modules['znc'] = znc
+
+import mock
+from notify_email import (should_notify, create_PrivMsg_email_subject, create_PrivMsg_email_body,
+                          create_ChanMsg_email_subject, create_ChanMsg_email_body, notify_email)
+
+def test_should_notify_should_return_true_if_msg_contains_thing():
+    assert should_notify(['foo', 'quux', 'xyzzy'], 'foo bar baz')
+    assert should_notify(['foo', 'quux', 'xyzzy'], 'bar foo baz')
+    assert should_notify(['foo', 'quux', 'xyzzy'], 'bar baz foo')
+
+def test_should_notify_should_return_false_if_msg_does_not_contain_thing():
+    assert not should_notify(['foo', 'quux', 'xyzzy'], 'bar baz')
+
+def test_create_PrivMsg_email_subject():
+    assert create_PrivMsg_email_subject('efnet', 'oyvindio') == \
+           'ZNC notify_email: oyvindio is trying to reach you on efnet'
+
+def test_create_PrivMsg_email_body():
+    expected = """
+    Hello,
+
+    oyvindio is trying to reach you on efnet, but you are currently AWAY.
+    Here is the message:
+
+    1970-01-01 00:00:00 <oyvindio>: the message
+
+    --
+    znc notify_email.py module
+    """
+    actual = create_PrivMsg_email_body('efnet', 'oyvindio', 'the message', '1970-01-01 00:00:00')
+    assert actual == expected
+
+def test_create_ChanMsg_email_subject():
+    assert create_ChanMsg_email_subject('efnet', 'oyvindio', '#channel') == \
+           'ZNC notify_email: oyvindio is trying to reach you in #channel on efnet'
+
+def test_create_ChanMsg_email_body():
+    expected = """
+    Hello,
+
+    oyvindio is trying to reach you in #channel on efnet, but you are currently AWAY.
+    Here is the message:
+
+    1970-01-01 00:00:00 <oyvindio>: the message
+
+    --
+    znc notify_email.py module
+    """
+    actual = create_ChanMsg_email_body('efnet', 'oyvindio', '#channel', 'the message', '1970-01-01 00:00:00')
+    assert actual == expected
+
+@mock.patch('znc.Module.GetNetwork')
+def test_isAway(GetNetwork):
+    notify_email().isAway()
+    GetNetwork.assert_has_calls([mock.call(), mock.call().IsIRCAway()])
+
+@mock.patch('znc.Module.GetNetwork')
+def test_currentNetworkName(GetNetwork):
+    notify_email().currentNetworkName()
+    GetNetwork.assert_has_calls([mock.call(), mock.call().GetName()])
+
+@mock.patch('znc.Module.PutModule')
+@mock.patch('requests.post')
+def test_send_mailgun_email_ok(post, PutModule):
+    base_url = 'http://example.com'
+    api_key = 'key'
+    sender = 'sender@example.com'
+    recipient = 'recipient@example.com'
+    subject = 'subject'
+    body = 'body'
+    message_id = 'message_id'
+
+    res = mock.MagicMock(ok=True, text='{"id":"%s"}' % message_id)
+    post.return_value = res
+
+    notify_email().send_mailgun_email(base_url, api_key, sender, recipient, subject, body)
+
+    post.assert_called_with(base_url + '/messages', auth=('api', api_key), data={
+        'from': sender,
+        'to': recipient,
+        'subject': subject,
+        'text': body
+    })
+    PutModule.assert_called_with('Successfully notified {}, mailgun id={}'.format(recipient, message_id))
+
+@mock.patch('znc.Module.PutModule')
+@mock.patch('requests.post')
+def test_send_mailgun_email_failure(post, PutModule):
+    base_url = 'http://example.com'
+    api_key = 'key'
+    sender = 'sender@example.com'
+    recipient = 'recipient@example.com'
+    subject = 'subject'
+    body = 'body'
+
+    status = 400
+    reason = 'Bad Request'
+    text = '{"error": "oh no!"}'
+    res = mock.MagicMock(ok=False, status=status, reason=reason, text=text)
+    post.return_value = res
+
+    notify_email().send_mailgun_email(base_url, api_key, sender, recipient, subject, body)
+
+    post.assert_called_with(base_url + '/messages', auth=('api', api_key), data={
+        'from': sender,
+        'to': recipient,
+        'subject': subject,
+        'text': body
+    })
+    PutModule.assert_called_with('Error while notifying {}, response from mailgun: {} {}\n{}'.format(
+        recipient, status, reason, text))
